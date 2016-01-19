e22021095141fe65fdf643ec3a61e3a09b530bc5
travismills82 <travismills82@gmail.com>
2016-01-19 16:10:42 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,21 +1,21 @@
-# Release name
-PRODUCT_RELEASE_NAME := trhpltexx
+# Inherit device configuration
+$(call inherit-product, device/samsung/trhpltexx/device.mk)
+$(call inherit-product, device/samsung/treltexx/device-common.mk)
 
-# Inherit some common EOS stuff.
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit nfc enhanced configuration
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/trhpltexx/device.mk)
-$(call inherit-product, device/samsung/treltexx/device-common.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_trhpltexx
 PRODUCT_DEVICE := trhpltexx
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-N910C
+PRODUCT_MODEL := SM-N910U
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
